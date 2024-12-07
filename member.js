18c6cfa3bcb49a11c948bee21126bffb0a7e5731
Mattia Barbiere <129879892+MattiaBarbiere@users.noreply.github.com>
2024-12-07 18:12:26 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,8 +1,3 @@
-function selectMember() {
-    var member = document.getElementById('member');
-    var memberName = member.options[member.selectedIndex].text;
-    var memberValue = member.options[member.selectedIndex].value;
-    var memberNameValue = memberName + " (" + memberValue + ")";
-    document.getElementById('memberName').value = memberNameValue;
-    document.getElementById('memberValue').value = memberValue;
+function skillsMember() {
+  console.log('skillsMember');
 }
